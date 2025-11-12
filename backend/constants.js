47969530fc90c1deae9c1629396cb67b75439118
Ashmit0405 <173537273+Ashmit0405@users.noreply.github.com>
2025-11-12 09:40:19 +0530

    Added the path

--- backend/constants.js
+++ backend/constants.js
@@ -4,4 +4,5 @@ export const options={
     httpOnly: true,
     secure: true,
     sameSite: "none",
+    path: "/"
 }
--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -4,9 +4,6 @@ console.log(import.meta.env.VITE_BACKEND_URL)
 const axiosInstance=axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL||"http://localhost:8000",
     withCredentials: true,
-    headers:{
-        "Content-Type":"application/json"
-    }
 })
 
 export default axiosInstance
